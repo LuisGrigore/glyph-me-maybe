88030c4ee35ae0320698ce3ecf50707514b7a667
Luis Grigore <64846391+LuisGrigore@users.noreply.github.com>
2026-09-11 12:59:35 +0200

    task: configured project to work with version.properties file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,17 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
 }
+val versionPropsFile = rootProject.file("version.properties")
+val versionProps = Properties()
+if (versionPropsFile.exists()) {
+    versionProps.load(FileInputStream(versionPropsFile))
+}
+val appVersionCode = versionProps.getProperty("versionCode", "1").toInt()
+val appVersionName = versionProps.getProperty("versionName", "1.0")
 
 android {
     namespace = "com.lcgg.glyph_me_maybe"
@@ -13,9 +23,8 @@ android {
         applicationId = "com.lcgg.glyph_me_maybe"
         minSdk = 33
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
-
+        versionCode = appVersionCode
+        versionName = appVersionName
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
